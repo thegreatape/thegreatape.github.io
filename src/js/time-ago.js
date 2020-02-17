04f03a87ceb66b19fff6ea0f445d0fa8ffebe715
Thomas Mayfield <Thomas.Mayfield@gmail.com>
2020-02-17 13:21:27 -0500

    Publishing at Mon Feb 17 13:21:27 EST 2020

---
+++ src/js/time-ago.js
@@ -0,0 +1,12 @@
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en';
+import $ from "cash-dom";
+
+TimeAgo.addLocale(en)
+
+$(function() {
+  const timeAgo = new TimeAgo('en-US');
+  $('.time-ago').map(function(_index, element){
+    $(element).html(timeAgo.format(new Date($(element).html())))
+  });
+});
